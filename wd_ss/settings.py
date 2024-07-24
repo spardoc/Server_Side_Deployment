aa37d80b4f9d38bebb1b1f3ba19ecaf41b42be3f
spardoc <samuelpardo1997gmail.com>
2024-07-23 22:35:07 -0500

    Implementación de librerías faltantes.

--- wd_ss/settings.py
+++ wd_ss/settings.py
@@ -28,9 +28,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://server-side-deployment.onrender.com']
 
 
 # Application definition
